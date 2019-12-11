8f44c6bcb4628ba492ace0c06378d10cbc519a7b
semicolonenter <semicolonenter@gmail.com>
2019-12-11 03:35:34 +0100

    covert to gray

--- project.py
+++ project.py
@@ -9,3 +9,7 @@ cv2.imshow("image", image)
 cv2.waitKey(0)
 
 #####################################################################################
+
+# convert to gray-scale
+grayInpaintedBlack = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+(_, thresh) = cv2.threshold(grayInpaintedBlack, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
